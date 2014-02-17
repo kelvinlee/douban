0e9e96f0ccdb84b228d983d06f4f9178b6b80397
Kelvin <kelvin.li@profero.com>
2014-02-17 18:19:32 +0800

    add Git.js

---
+++ git.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var url = require('url');
+var exec = require("child_process").exec;
+var port = 9998;
+
+function gitpull(porject) { 
+  exec("cd /my/demo/douban && git pull");
+  return true;
+}
+function routes(req,res) {
+  var pathname = url.parse(req.url).pathname;
+  if (pathname==="/update" && req.method.toLowerCase()=="post") { 
+    gitpull();
+    console.log("I need run git.");
+  }else{
+    return "404";
+  }
+}
+var req = http.createServer(function(req,res){
+  res.writeHead(200, {'Content-Type': 'text/plain'}); 
+  routes(req,res);
+  // exec("ls");
+  res.end(); 
+}).listen(port);
+console.log("Git Douban start.");
